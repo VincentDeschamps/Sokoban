b45b379612fab5cff2c82942b69a04fa90f152e1
Martin <martinschultz37@gmail.com>
2018-11-27 10:02:46 +0100

    Creation classe mur

---
+++ src/sample/Mur.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Mur implements GameObject{
+
+    int posX;
+    int posY;
+    Modele m;
+
+    Mur(int posX, int posY){
+        this.posX = posX;
+        this.posY = posY;
+    }
+}
