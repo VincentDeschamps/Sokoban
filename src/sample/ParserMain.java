a7b5f20d08be243f5ea17e68d9e4d9e28e8dde36
Sheikkin <shikicube@gmail.com>
2018-12-04 10:52:03 +0100

    Parsing map > ArraList<ArrayList<Character>> effectif

--- src/sample/ParserMain.java
+++ src/sample/ParserMain.java
@@ -7,12 +7,15 @@ public class ParserMain {
 
     public static void main (String[] args){
         String fic = "/home/etud/o2163473/Bureau/map1.xsb";
-        ArrayList<String> map;
+        ArrayList<ArrayList<Character>> map;
         try {
             map = MapParser.parseMap(fic);
+            System.out.println(map);
+            System.out.println("File found");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
     }
 }
 
